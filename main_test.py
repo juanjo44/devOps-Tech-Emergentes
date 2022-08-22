78c69218ec08d0e3bc9a7666bd92b0f36a1870b0
juanjo44 <juanjosebetancourtosorio@gmail.com>
2022-08-22 00:07:58 -0500

    Second test(it's failing

--- main_test.py
+++ main_test.py
@@ -8,3 +8,7 @@ client = TestClient(app)
 def test_index_route():
     response = client.get('/items/1')
     assert response.status_code == 200
+    
+def test_index_route_failing():
+    response = client.get('/items/1000')
+    assert response.status_code == 400
